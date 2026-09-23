f2847ed167fa4fea06bf0083739b5f92c94f30ce
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-23 15:22:40 -0400

    RabeyaMuna__diff__id_348

--- aiohttp/web_protocol.py
+++ aiohttp/web_protocol.py
@@ -160,7 +160,7 @@ class RequestHandler(BaseProtocol):
         "_upgrade",
         "_payload_parser",
         "_request_parser",
-        "_reading_paused",
+
         "logger",
         "debug",
         "access_log",
